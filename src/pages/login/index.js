278b4407cf632193c5fad2784da4622e883e4ff4
cc <1285268953@qq.com>
2022-12-06 23:30:55 +0800

    fix: phone number validation & question number

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -6,7 +6,7 @@ import './index.css'
 export default function Login() {
   let navigate = useNavigate()
   const [visible, setVisible] = useState(false)
-  const regExp = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/
+  const regExp = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57]|199)[0-9]{8}$/
 
   const handleSubmit = (e) => {
     // console.log('success', e)
--- src/pages/soduku/index.js
+++ src/pages/soduku/index.js
@@ -5,30 +5,25 @@ import moment, { Moment } from 'moment'
 import { useNavigate } from 'react-router-dom'
 import './index.css'
 
-const testData1 =
-  ',1,,,,,2,0,,4,,7,,1,,,,,,,,4,,,,8,,7,,6,,,,,2,,3,,,,6,5,,,,,,,,,,,,3,,,5,,,3,,,,,,8,,,,,,7,6,,,8,,,4,,'
-const testData2 =
-  ',,,,,,,,2,,,,,,,5,3,,5,8,3,,,,,4,,,7,,2,,6,,8,,,0,,7,,,3,,,,,5,,,,6,,4,8,2,,3,,,,,,,,,,,0,,,,,1,7,,,4,8,,'
-
 export default function SudokuPage() {
   let navigate = useNavigate()
 
   const [loading, setLoading] = useState(true)
   const [notArrive, setNotArrive] = useState(true)
   const [question1, setQuestion1] = useState("")
-  const [question2, setQuestion2] = useState("")
+  // const [question2, setQuestion2] = useState("")
   const [answer1, setAnswer1] = useState(question1)
-  const [answer2, setAnswer2] = useState(question2)
+  // const [answer2, setAnswer2] = useState(question2)
   const [now, setNow] = useState(moment()) // 会返回当前状态的属性 和修改状态的方法
   const [submit, setSubmit] = useState(false)
   
   const onConfirm = () => {
     setLoading(true)
     console.log('a1', answer1)
-    console.log('a2', answer2)
+    // console.log('a2', answer2)
     let data=new FormData()
     data.append('paper1',answer1)
-    data.append('paper2',answer2)
+    // data.append('paper2',answer2)
     console.log(data)
     fetch('https://sudokuserver.boxz.dev/api/submitpaper/', {
       method: 'POST',
@@ -64,14 +59,14 @@ export default function SudokuPage() {
         try{
           const jsonRes = await res.json()
           setQuestion1(jsonRes.data.paper1.content)
-          setQuestion2(jsonRes.data.paper2.content)
+          // setQuestion2(jsonRes.data.paper2.content)
           
         } catch(e) {
           alert(e)
         }
       }
       console.log("q1\n", question1)
-      console.log("q2\n", question2)
+      // console.log("q2\n", question2)
     }
     fetchData()
     setLoading(false)
@@ -138,7 +133,7 @@ export default function SudokuPage() {
             <Sudoku puzzle={question1} setAnswer={setAnswer1} />
           </div>
         </TabPane>
-        <TabPane tab="题目2" itemKey="2">
+        {/* <TabPane tab="题目2" itemKey="2">
           <div
             style={{
               display: 'flex',
@@ -148,9 +143,8 @@ export default function SudokuPage() {
           >
             <Sudoku puzzle={question2} setAnswer={setAnswer2} />
           </div>
-        </TabPane>
+        </TabPane> */}
       </Tabs>
-      {/* <Time /> */}
       <div className="right-sider">
         <div style={{fontSize: '16px', fontWeight: '600'}}>北京时间</div>
         <div className='timer'>{now.format('HH:mm:ss')}</div>
